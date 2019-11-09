b6a61132dade92ddf18e4099c2e12afb8656c6c5
danikkm <40919040+danikkm@users.noreply.github.com>
2019-11-09 15:46:06 +0200

    Code cleanup + PEP 8 Naming Conventions

--- SystemHelper.py
+++ SystemHelper.py
@@ -2,15 +2,32 @@
 import os
 
 
-def flushScreen():
-    try:
+class SystemHelper:
+
+    @staticmethod
+    def flush_screen():
         try:
-            os.system('clear')
+            try:
+                os.system('clear')
+            except:
+                os.system('cls')
         except:
-            os.system('cls')
-    except:
-        print('\n' * 80)
+            print('\n' * 80)
 
-
-
-
+    @staticmethod
+    def get_key_strokes():
+        return {
+            "W": 87,
+            "S": 83,
+            "A": 65,
+            "D": 68,
+            "w": 119,
+            "s": 115,
+            "a": 97,
+            "d": 100,
+            "q": 113,
+            "upArrow": 65,
+            "downArrow": 66,
+            "leftArrow": 68,
+            "rightArrow": 67
+        }
